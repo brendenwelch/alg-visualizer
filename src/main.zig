46e051343bb8acd85ac1907fb877f461502d9295
brendenwelch <aloofd@gmail.com>
2025-05-29 22:27:59 -0700

    Finish cleanup. Need to choose new allocator, setup error handling and logging.

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,15 @@
-const sdl3 = @import("sdl3");
+const sdl = @import("sdl3");
 const std = @import("std");
-
-// https://www.pexels.com/photo/green-trees-on-the-field-1630049/
-const my_image = @embedFile("data/trees.jpg");
 
 const WINDOW_WIDTH = 640;
 const WINDOW_HEIGHT = 480;
 
-/// Allocator we will use.
-/// You probably want a different one for your applications.
+/// May want to use different allocator.
 const allocator = std.heap.c_allocator;
 
-/// Sample structure to use to hold our app state.
 const AppState = struct {
-    window: sdl3.video.Window,
-    renderer: sdl3.render.Renderer,
-    tree_tex: sdl3.render.Texture,
+    window: sdl.video.Window,
+    renderer: sdl.render.Renderer,
 };
 
 /// Do our initialization logic here.
@@ -29,7 +23,7 @@ const AppState = struct {
 pub fn init(
     app_state: *?*AppState,
     args: [][*:0]u8,
-) !sdl3.AppResult {
+) !sdl.AppResult {
     _ = args;
 
     // Prepare app state.
@@ -37,7 +31,7 @@ pub fn init(
     errdefer allocator.destroy(state);
 
     // Setup initial data.
-    const window_renderer = try sdl3.render.Renderer.initWithWindow(
+    const window_renderer = try sdl.render.Renderer.initWithWindow(
         "Algorithm Visualizer",
         WINDOW_WIDTH,
         WINDOW_HEIGHT,
@@ -45,18 +39,11 @@ pub fn init(
     );
     errdefer window_renderer.renderer.deinit();
     errdefer window_renderer.window.deinit();
-    const tree_tex = try sdl3.image.loadTextureIo(
-        window_renderer.renderer,
-        try sdl3.io_stream.Stream.initFromConstMem(my_image),
-        true,
-    );
-    errdefer tree_tex.deinit();
 
     // Set app state.
     state.* = .{
         .window = window_renderer.window,
         .renderer = window_renderer.renderer,
-        .tree_tex = tree_tex,
     };
     app_state.* = state;
 
@@ -72,16 +59,9 @@ pub fn init(
 /// Returns if the app should continue running, or result in success or failure.
 pub fn iterate(
     app_state: *AppState,
-) !sdl3.AppResult {
+) !sdl.AppResult {
     try app_state.renderer.setDrawColor(.{ .r = 128, .g = 30, .b = 255 });
     try app_state.renderer.clear();
-    const border = 10;
-    try app_state.renderer.renderTexture(app_state.tree_tex, null, .{
-        .x = border,
-        .y = border,
-        .w = WINDOW_WIDTH - border * 2,
-        .h = WINDOW_HEIGHT - border * 2,
-    });
     try app_state.renderer.present();
     return .run;
 }
@@ -96,8 +76,8 @@ pub fn iterate(
 /// Returns if the app should continue running, or result in success or failure.
 pub fn eventHandler(
     app_state: *AppState,
-    event: sdl3.events.Event,
-) !sdl3.AppResult {
+    event: sdl.events.Event,
+) !sdl.AppResult {
     _ = app_state;
     switch (event) {
         .terminating => return .success,
@@ -114,11 +94,10 @@ pub fn eventHandler(
 /// * `result`: Result indicating the success of the application. Should never be `AppResult.run`.
 pub fn quit(
     app_state: ?*AppState,
-    result: sdl3.AppResult,
+    result: sdl.AppResult,
 ) void {
     _ = result;
     if (app_state) |val| {
-        val.tree_tex.deinit();
         val.renderer.deinit();
         val.window.deinit();
         allocator.destroy(val);
