4bcca3eb3f9f5c28703a20bacfd3f268df809bae
brendenwelch <aloofd@gmail.com>
2025-05-30 21:44:54 -0700

    let's try from scratch

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "alg_visualizer",
-        .root_source_file = b.path("src/entry.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -31,7 +31,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/entry.zig"),
+        .root_source_file = b.path("src/main.zig"),
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
